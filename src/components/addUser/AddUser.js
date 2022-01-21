4ffaa9752467fa96b5eb85c38054bba807660f2a
faisal8373 <59798752+faisal8373@users.noreply.github.com>
2022-01-21 22:11:35 +0500

    Update AddUser.js

--- src/components/addUser/AddUser.js
+++ src/components/addUser/AddUser.js
@@ -3,8 +3,22 @@ import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import "./AddUserStyle.css";
+import { useState } from "react";
+import Alert from '@mui/material/Alert';
 
 function AddUser() {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [age, setAge] = useState(0);
+
+  function ctaHandler() {
+    console.log(userName, email, mobile, age);
+    setUserName("");
+    setEmail("");
+    setMobile("");
+    setAge("");
+  }
   return (
     <div>
       <form className="form">
@@ -12,44 +26,59 @@ function AddUser() {
         <TextField
           className="text-field"
           label="Name"
-          name="name"
+          value={userName}
           margin="normal"
           type="text"
           variant="outlined"
           required
-          fullWidth 
+          fullWidth
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
         />
         <TextField
           className="text-field"
           label="Email"
+          value={email}
           margin="normal"
           type="email"
           variant="outlined"
           required
           fullWidth
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
         />
         <TextField
           className="text-field"
           label="Mobile"
+          value={mobile}
           margin="normal"
           type="text"
           variant="outlined"
           required
           fullWidth
+          onChange={(e) => {
+            setMobile(e.target.value);
+          }}
         />
         <TextField
           className="text-field"
           label="Age"
+          value={age}
           margin="normal"
           type="number"
-          min="18"
-          max="60"
+          InputProps={{ inputProps: { min: 18, max: 60 } }}
           variant="outlined"
           required
           fullWidth
+          onChange={(e) => {
+            setAge(e.target.value);
+          }}
         />
+
         <br />
-        <Button type="submit" variant="contained" fullWidth >
+        <Button variant="contained" fullWidth onClick={ctaHandler}>
           Submit
         </Button>
       </form>
